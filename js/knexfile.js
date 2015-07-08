73ddf7deef4ca4842e362e2fb975cb473d2df3dc
Morgan Leonard <morganleonardpublic@gmail.com>
2015-07-07 17:28:34 -0700

    updated knexfile.js to use ElephantSQL database

--- js/knexfile.js
+++ js/knexfile.js
@@ -1,17 +1,20 @@
-// Update with your config settings.
+var config = require('./config.js');
 
 module.exports = {
 
-  client: 'postgresql',
-  connection: {
-    database: 'calendar',
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
+ client: 'postgresql',
+ connection: {
+ host     : process.env.APP_DB_HOST     || config.dbHost,
+ user     : process.env.APP_DB_USER     || config.dbUser,
+ password : process.env.APP_DB_PASSWORD || config.dbPassword,
+ database : process.env.APP_DB_NAME     || config.dbName
+},
+ pool: {
+   min: 2,
+   max: 10
+ },
+ migrations: {
+   tableName: 'knex_migrations'
+ }
 
 };
